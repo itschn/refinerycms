ed05ac01d6b79bfc803df311a8c7203ac9cfcef4
itschn <lofd23@yahoo.de>
2013-10-17 16:07:38 +0200

    Adjusting carrierwave immage model

--- images/app/helpers/refinery/admin/images_helper.rb
+++ images/app/helpers/refinery/admin/images_helper.rb
@@ -10,13 +10,9 @@ module Refinery
       def thumbnail_urls(image)
         thumbnail_urls = {
           :"data-original" => asset_paths.compute_public_path(image.url, ''),
+          :"data-large" => asset_paths.compute_public_path(image.url, ''),
           :"data-grid" => asset_paths.compute_public_path(image.url(:thumb), '')
         }
-
-        Refinery::Images.user_image_sizes.sort_by{|key,geometry| geometry}.each do |size, pixels|
-          thumbnail_urls[:"data-#{size.to_s.parameterize}"] = asset_paths.compute_public_path(image.url(:thumb), '')
-        end
-
         thumbnail_urls
       end
     end
