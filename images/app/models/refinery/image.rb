92cba58a47ffdc678329c431488f7bd036d0edcd
itschn <lofd23@yahoo.de>
2013-10-17 14:11:31 +0200

    Adjusting carrierwave immage model

--- images/app/models/refinery/image.rb
+++ images/app/models/refinery/image.rb
@@ -21,5 +21,17 @@ module Refinery
     def title
       image_name
     end
+    
+    def self.per_page(dialog = false, has_size_options = false)
+      if dialog
+        unless has_size_options
+          Images.pages_per_dialog
+        else
+          Images.pages_per_dialog_that_have_size_options
+        end
+      else
+        Images.pages_per_admin_index
+      end
+    end
   end
 end
